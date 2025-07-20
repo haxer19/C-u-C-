85ba56916a2e3d5ca01faa5d2b0483a2c9a00868
Kiz <caotienthanh924@gmail.com>
2025-07-20 10:45:09 +0700

    TienThanh
    Câu cá PNV

--- updater.py
+++ updater.py
@@ -1,7 +1,7 @@
 import requests,os,zipfile,shutil
 
 GITHUB_REPO_URL = "https://github.com/haxer19/C-u-C-"
-RAW_VERSION_URL = "https://github.com/haxer19/C-u-C-/main/version.txt"
+RAW_VERSION_URL = "https://raw.githubusercontent.com/haxer19/C-u-C-/main/version.txt"
 ZIP_URL = "https://github.com/haxer19/C-u-C-/archive/refs/heads/main.zip"
 
 LOCAL_VERSION_FILE = "version.txt"
